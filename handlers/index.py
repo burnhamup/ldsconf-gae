19cef4b6a91d9b9d8e08b0031762d1174f40a162
Chris Burnham <chris@burnhamup.com>
2015-12-27 14:48:30 -0800

    Simple Error handling

--- handlers/index.py
+++ handlers/index.py
@@ -13,18 +13,27 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 GAE_CONFERENCE_FILE = os.path.join(os.path.dirname(__file__), '..', 'data', 'conferences.json')
 
+
 class IndexHandler(webapp2.RequestHandler):
   def get(self):
     template = JINJA_ENVIRONMENT.get_template('index.html')
     self.response.write(template.render({}))
   
   def post(self):
-    start_date_raw = self.request.get('start_date')
-    end_date_raw = self.request.get('end_date')
-    start_date = datetime.strptime(start_date_raw, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date_raw, '%Y-%m-%d')
-    number_of_plans = int(self.request.get('number_of_plans'))
+    try:
+      start_date_raw = self.request.get('start_date')
+      end_date_raw = self.request.get('end_date')
+      start_date = datetime.strptime(start_date_raw, '%Y-%m-%d')
+      end_date = datetime.strptime(end_date_raw, '%Y-%m-%d')
+      number_of_plans = int(self.request.get('number_of_plans'))
+    except:
+      template = JINJA_ENVIRONMENT.get_template('error.html')
+      self.response.write(template.render({}))
+      return
+
+
     result = ldsconf.studyplan.generate_study_plan(start_date, end_date, number_of_plans, GAE_CONFERENCE_FILE)
+
     template = JINJA_ENVIRONMENT.get_template('plan.html')
     self.response.write(template.render({'plans': result}))
 
